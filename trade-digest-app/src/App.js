3a8a6d71c04489aa33af27971431d375a5569a06
maya486 <maya.rebholz@gmail.com>
2022-07-08 18:17:46 -0400

    rough-draft-minus-schedule

--- trade-digest-app/src/App.js
+++ trade-digest-app/src/App.js
@@ -1,5 +1,7 @@
 import "./App.css";
-// import { logo } from "./logo.svg";
+import { BiGift } from "react-icons/bi";
+import { FiExternalLink } from "react-icons/fi";
+import { TbCircleCheck } from "react-icons/tb";
 import {
   Text,
   ChakraProvider,
@@ -19,10 +21,14 @@ import {
   AccordionPanel,
   AccordionIcon,
   Tag,
+  TagLabel,
+  TagLeftIcon,
+  Icon,
   Stack,
   Spacer,
   VStack,
 } from "@chakra-ui/react";
+import { CheckIcon } from "@chakra-ui/icons";
 
 function App() {
   const date = "Fri Jun 10, 2022";
@@ -55,7 +61,8 @@ function App() {
         </div>
         <LinkBox className="punch-item-link">
           <LinkOverlay href={href} />
-          <Button id="punch-item-button" />
+          {/* <div id="punch-item-button">[Icon]</div> */}
+          <Icon as={FiExternalLink} w="23px" h="23px" />
         </LinkBox>
       </div>
     );
@@ -103,11 +110,36 @@ function App() {
       </>
     );
   };
+  const MTButton = ({ date, info }) => {
+    return (
+      <>
+        <div className="wov-button-wrapper">
+          {/* <div id="punch-item-button" fontSize="14px">
+            [Icon]
+          </div> */}
+          <Icon as={TbCircleCheck} w="17px" h="17px" />
+          <Text className="mt-button-date">{date}</Text>
+          <Text className="mt-button-info">{info}</Text>
+          <></>
+        </div>
+        <AccordionIcon />
+      </>
+    );
+  };
   const CustomAccordionButton = ({ id, loc }) => {
     return (
       <h2>
         <AccordionButton fontSize="14px">
           <WOVButton id={id} loc={loc} />
+        </AccordionButton>
+      </h2>
+    );
+  };
+  const CustomMTAccordionButton = ({ date, info }) => {
+    return (
+      <h2>
+        <AccordionButton fontSize="14px">
+          <MTButton date={date} info={info} />
         </AccordionButton>
       </h2>
     );
@@ -126,6 +158,19 @@ function App() {
       </>
     );
   };
+  const MTPanel = ({ code, quantity, id }) => {
+    return (
+      <>
+        <div className="mt-panel-wrapper">
+          <p id="code">Item Code: {code}</p>
+          <p id="quantity">Qty: {quantity}</p>
+        </div>
+        <div className="mt-panel-wrapper">
+          <p id="id">Tracking ID: {id}</p>
+        </div>
+      </>
+    );
+  };
   const CustomAccordionPanel = ({ title, plan, item, elevation }) => {
     return (
       <AccordionPanel
@@ -138,6 +183,104 @@ function App() {
       >
         <WOVPanel title={title} plan={plan} item={item} elevation={elevation} />
       </AccordionPanel>
+    );
+  };
+  const CustomMTAccordionPanel = ({ code, quantity, id }) => {
+    return (
+      <AccordionPanel
+        bg="#F2F0ED"
+        css={{
+          margin: 0,
+          padding: "5px 20px",
+          borderTop: "1px solid #DBDDE1",
+        }}
+      >
+        <MTPanel code={code} quantity={quantity} id={id} />
+      </AccordionPanel>
+    );
+  };
+  const RewardItem = ({ name, isClaimed, num }) => {
+    return (
+      <>
+        <div className="reward-item-wrapper">
+          <div className="reward-item-text">
+            <p id="name">{name}</p>
+            <p id="fulfill">Fulfill {num} Punch Items</p>
+          </div>
+          <div className="reward-item-tag">
+            {isClaimed ? (
+              <Tag borderRadius="2px" bg="#DFF1EC">
+                <TagLeftIcon
+                  className="check-icon"
+                  boxSize="12px"
+                  as={CheckIcon}
+                  color="#1CC38D"
+                />
+                <TagLabel className="claim-label">Claimed</TagLabel>
+              </Tag>
+            ) : (
+              <Tag borderRadius="2px" bg="#FBF9F6">
+                <TagLabel className="claim-label">Claim</TagLabel>
+              </Tag>
+            )}
+          </div>
+        </div>
+      </>
+    );
+  };
+  const DemoAccordion = () => {
+    return (
+      <Accordion allowMultiple>
+        <AccordionItem>
+          <CustomAccordionButton id="KER:201986" loc="Jasper 1C | Lot 5" />
+          <CustomAccordionPanel
+            title="Draw 1 - Material"
+            plan="Plan: 5-Plex"
+            item="Item/Notes"
+            elevation="Elevation:-"
+          />
+        </AccordionItem>
+
+        <AccordionItem>
+          <CustomAccordionButton id="KER:201987" loc="Jasper 1C | Lot 5" />
+          <CustomAccordionPanel
+            title="Draw 2 - Labor"
+            plan="Plan: 5-Plex"
+            item="Item/Notes"
+            elevation="Elevation:-"
+          />
+        </AccordionItem>
+        <AccordionItem>
+          <CustomAccordionButton id="KER:201988" loc="Jasper 1C | Lot 5" />
+          <CustomAccordionPanel
+            title="Draw 1 - Material"
+            plan="Plan: 5-Plex"
+            item="Item/Notes"
+            elevation="Elevation:-"
+          />
+        </AccordionItem>
+        <AccordionItem>
+          <CustomAccordionButton id="KER:201989" loc="Jasper 1C | Lot 5" />
+          <CustomAccordionPanel
+            title="Draw 2 - Labor"
+            plan="Plan: 5-Plex"
+            item="Item/Notes"
+            elevation="Elevation:-"
+          />
+        </AccordionItem>
+      </Accordion>
+    );
+  };
+  const StartLetter = ({ loc, date, href }) => {
+    return (
+      <div className="start-letter-wrapper">
+        <Text id="start-letter-loc">{loc}</Text>
+        <Text id="start-letter-date">Last Updated: {date}</Text>
+        <LinkBox className="punch-item-link">
+          <LinkOverlay href={href} />
+          <Icon as={FiExternalLink} w="17px" h="17px" />
+        </LinkBox>
+      </div>
     );
   };
   return (
@@ -164,7 +307,7 @@ function App() {
             timestamp="6 hours ago"
           />
         </Box>
-        <ReportHeader text="Upcoming Pay Period" />
+        <ReportHeader text="This Pay Period" />
         <div className="upp-wrapper">
           <CircularProgress
             id="circle-progress"
@@ -187,86 +330,121 @@ function App() {
             <PunchItem
               name="Inspect footing for cracks"
               details="Jasper 1C | Lot 5"
+              href="#"
             />
           </Box>
           <Box className="punch-item-box" boxShadow={"lg"}>
             <PunchItem
               name="Framing inspection failed, fix some anchor bolts"
               details="Jasper 1C | Lot 5"
+              href="#"
             />
           </Box>
         </div>
-        <ReportHeader text="Work Orders and VPOs" />
-        <Tabs className="work-tabs" isFitted variant="line">
+        <ReportHeader text="Scope of Work" />
+        <Tabs className="work-tabs" variant="line">
           <TabList css={{ border: "1px solid #DCDFE2" }}>
+            <CustomTab text="Start Letter" />
             <CustomTab text="Work Orders" />
             <CustomTab text="VPOs" />
             <CustomTab text="Approved" />
           </TabList>
-
+          <TabPanels>
+            <TabPanel css={{ margin: 0, padding: 0 }}>
+              <StartLetter loc="Jasper 1C | Lot 5" date="07/07/2022" href="#" />
+              <StartLetter loc="Jasper 1C | Lot 6" date="07/07/2022" href="#" />
+              <StartLetter loc="Jasper 1C | Lot 7" date="07/07/2022" href="#" />
+              <StartLetter loc="Jasper 1C | Lot 8" date="07/07/2022" href="#" />
+              <StartLetter loc="Jasper 1C | Lot 9" date="07/07/2022" href="#" />
+            </TabPanel>
+            <TabPanel css={{ margin: 0, padding: 0 }}>
+              <DemoAccordion />
+            </TabPanel>
+            <TabPanel css={{ margin: 0, padding: 0 }}>
+              <DemoAccordion />
+            </TabPanel>
+            <TabPanel css={{ margin: 0, padding: 0 }}>
+              <DemoAccordion />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+        <ReportHeader text="Material Tracking" />
+        <Tabs className="work-tabs" variant="line">
+          <TabList css={{ border: "1px solid #DCDFE2" }}>
+            <CustomTab text="Delivered Items" />
+            <CustomTab text="Pending Items" />
+          </TabList>
           <TabPanels>
             <TabPanel css={{ margin: 0, padding: 0 }}>
               <Accordion allowMultiple>
                 <AccordionItem>
-                  <CustomAccordionButton
-                    id="KER:201986"
-                    loc="Jasper 1C | Lot 5"
+                  <CustomMTAccordionButton
+                    date="June 7, 2022"
+                    info="1/2-in Plywood for headers"
                   />
-                  <CustomAccordionPanel
-                    title="Draw 1 - Material"
-                    plan="Plan 5-Plex"
-                    item="Item/Notes"
-                    elevation="Elevation:-"
-                  />
-                </AccordionItem>
-
-                <AccordionItem>
-                  <CustomAccordionButton
-                    id="KER:201987"
-                    loc="Jasper 1C | Lot 5"
-                  />
-                  <CustomAccordionPanel
-                    title="Draw 2 - Labor"
-                    plan="Plan 5-Plex"
-                    item="Item/Notes"
-                    elevation="Elevation:-"
+                  <CustomMTAccordionPanel
+                    code="FM4505"
+                    quantity="2500"
+                    id="524917889015W4"
                   />
                 </AccordionItem>
                 <AccordionItem>
-                  <CustomAccordionButton
-                    id="KER:201988"
-                    loc="Jasper 1C | Lot 5"
+                  <CustomMTAccordionButton
+                    date="June 7, 2022"
+                    info="16d framing nails"
                   />
-                  <CustomAccordionPanel
-                    title="Draw 1 - Material"
-                    plan="Plan 5-Plex"
-                    item="Item/Notes"
-                    elevation="Elevation:-"
+                  <CustomMTAccordionPanel
+                    code="FM4505"
+                    quantity="2500"
+                    id="524917889015W4"
                   />
                 </AccordionItem>
                 <AccordionItem>
-                  <CustomAccordionButton
-                    id="KER:201989"
-                    loc="Jasper 1C | Lot 5"
+                  <CustomMTAccordionButton
+                    date="June 7, 2022"
+                    info="8d framing nails"
                   />
-                  <CustomAccordionPanel
-                    title="Draw 2 - Labor"
-                    plan="Plan 5-Plex"
-                    item="Item/Notes"
-                    elevation="Elevation:-"
+                  <CustomMTAccordionPanel
+                    code="FM4505"
+                    quantity="2500"
+                    id="524917889015W4"
+                  />
+                </AccordionItem>
+                <AccordionItem>
+                  <CustomMTAccordionButton
+                    date="May 29, 2022"
+                    info="2 x 6 studs"
+                  />
+                  <CustomMTAccordionPanel
+                    code="FM4505"
+                    quantity="2500"
+                    id="524917889015W4"
                   />
                 </AccordionItem>
               </Accordion>
             </TabPanel>
             <TabPanel>
-              <p>two!</p>
-            </TabPanel>
-            <TabPanel>
-              <p>three!</p>
+              <p>idk</p>
             </TabPanel>
           </TabPanels>
         </Tabs>
-        <ReportHeader text="Material Tracker" />
+        <ReportHeader text="Rewards" />
+        <div className="rewards-wrapper">
+          <Icon as={BiGift} w={40} h={40} color="#27364F" padding="20px" />
+          <p id="rewards-description">
+            EARN REWARDS
+            <br /> by fulfilling punch items!
+          </p>
+          <div className="punch-items-remaining-wrapper">
+            <p id="remaining-number">29</p>
+            <p id="remaining-text">Punch Items Fulfilled</p>
+          </div>
+        </div>
+        <Divider />
+        <RewardItem isClaimed={true} num="5" name="Gift Card #1" />
+        <Divider />
+        <RewardItem isClaimed={false} num="10" name="Gift Card #2" />
+        <Divider />
       </div>
     </ChakraProvider>
   );
